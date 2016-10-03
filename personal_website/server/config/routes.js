e05056d5704d92874c03752f26998633470613f9
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-10-02 18:11:56 -0700

    completed 'Daily Plan' page (including styling)

--- personal_website/server/config/routes.js
+++ personal_website/server/config/routes.js
@@ -1,6 +1,7 @@
 var contacts = require("./../controllers/contacts");
 var users = require("./../controllers/users");
 var questions = require("./../controllers/questions");
+var planners = require("./../controllers/planners");
 
 module.exports = function(app) {
 
@@ -17,4 +18,10 @@ module.exports = function(app) {
 	app.put("/question/:id", questions.update_question);
 	app.delete("/question/:id", questions.delete_question);
 
+	// [RESTful] deals with http requests from the 'Daily Plan' page
+	app.post("/planner", planners.create_planner);
+	app.get("/planner", planners.index_planner);
+	app.put("/planner/:id", planners.update_planner);
+	app.patch("/planner/:id", planners.delete_single_plan);
+	app.delete("/planner/:id", planners.delete_planner);
 }
